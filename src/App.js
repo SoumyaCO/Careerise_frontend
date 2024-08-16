a916e477e4b1c6c8da88dabbd64b03ba1389d564
rittima <rittimabiswas@gmail.com>
2024-08-16 12:45:47 +0530

    5th commit with React project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 
-import Home1 from "./rittima/home/Home1";
-import Home2 from "./rittima/home/Home2";
-
-// import Register from "../rittima/register/Register";
-// import Login from "../rittima/login/Login";
+import Home1 from "./compotent1/home/Home1";
+import Home2 from "./compotent1/home/Home2";
+// import Register from "../compotent1/register/Register";
+// import Login from "../compotent1/login/Login";
+// import Component1 from "./compotent1/Component1";
 
 function App() {
   
